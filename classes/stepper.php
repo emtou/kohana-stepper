3ef5d6b30d297efb7d2529b5b828328f39f843cd
mtou <mtou@charougna.com>
2011-09-13 11:30:05 +0200

    Create empty stepper definition

---
+++ classes/stepper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Stepper helper
+ *
+ * PHP version 5
+ *
+ * @group stepper
+ *
+ * @category  Stepper
+ * @package   Stepper
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-stepper/tree/master/classes/stepper.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Stepper helper
+ *
+ * PHP version 5
+ *
+ * @group stepper
+ *
+ * @category  Stepper
+ * @package   Stepper
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-stepper/tree/master/classes/stepper.php
+ */
+class Stepper extends Core_Stepper
+{
+}
