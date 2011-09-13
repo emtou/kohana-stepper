3cc114e6e5e1a8eb56c5d0b9a637154c7171f690
mtou <mtou@charougna.com>
2011-09-13 11:43:06 +0200

    Add basic functionnalities

---
+++ views/stepper/default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Declares default Stepper view
+ *
+ * PHP version 5
+ *
+ * @group stepper
+ *
+ * @category  Stepper
+ * @package   Stepper
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-stepper/tree/master/views/stepper/default.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+foreach ($steps as $step)
+{
+  echo "step";
+}
